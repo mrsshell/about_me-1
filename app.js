85cc9606bc466470b35bf3919cb70c50986d87c3
Matt Favoino <mattfavoino@gmail.com>
2017-08-08 14:45:15 -0700

    Updated js and html files

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var pButter = prompt('Do you like peanut butter?').toLowerCase();
+console.log('Q: Do you like peanut butter?');
+console.log('A: ' + pButter);
+if (pButter === 'y' || pButter === 'yes') {
+  alert('Nice! That\'s my favorite food.');
+} else {
+  alert('You\'re missing out.');
+}
