b9a3aa03439757041a68fa530bbc20fa4e300bb9
Matt Favoino <mattfavoino@gmail.com>
2017-08-09 09:49:49 -0700

    js file updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//change .toLowerCase() to be included within the if statements
 var pButter = prompt('Do you like peanut butter?').toLowerCase();
 console.log('Q: Do you like peanut butter?');
 console.log('A: ' + pButter);
